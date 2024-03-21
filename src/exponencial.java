4b9f7107530aa1e128cd0f37b3884b77b3779929
Gustavo Calderon Perez <tuticp04@gmail.com>
2024-03-20 19:46:21 -0600

    exponencial

--- src/exponencial.java
+++ src/exponencial.java
@@ -1,15 +1,15 @@
 public class exponencial {
 
     public static void main(String[] args) {
-        long startTime = System.currentTimeMillis();
+        long startTime = System.nanoTime();
 
-        int n = 100; // The index of the Fibonacci number to calculate
-        System.out.println("Fibonacci number at index " + n + ": " + fibonacci(n));
+        int n = 30; // The index of the Fibonacci number to calculate
+        System.out.println(fibonacci(n));
 
-        long endTime = System.currentTimeMillis();
+        long endTime = System.nanoTime();
         long durationSeconds = (endTime - startTime);
 
-        System.out.println("Time taken: " + durationSeconds + " miliseconds");
+        System.out.println("Time taken: " + durationSeconds + " nanoseconds");
     }
 
     public static int fibonacci(int n) {
