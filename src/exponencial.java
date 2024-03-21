ae4891ac3d1af1ef277724dcacca837d226ae3d5
Tuti004 <tuticp04@gmail.com>
2024-03-20 18:51:27 -0600

    fibonacci

--- src/exponencial.java
+++ src/exponencial.java
@@ -1,2 +1,22 @@
 public class exponencial {
+
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+
+        int n = 100; // The index of the Fibonacci number to calculate
+        System.out.println("Fibonacci number at index " + n + ": " + fibonacci(n));
+
+        long endTime = System.currentTimeMillis();
+        long durationSeconds = (endTime - startTime);
+
+        System.out.println("Time taken: " + durationSeconds + " miliseconds");
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
 }
