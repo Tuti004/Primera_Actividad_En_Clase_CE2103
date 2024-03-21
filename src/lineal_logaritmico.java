8f72d86d5d16ce0984a9b551bcfd953c01b848ae
Gustavo Calderon Perez <tuticp04@gmail.com>
2024-03-20 20:05:39 -0600

    lineal_logaritmico

--- src/lineal_logaritmico.java
+++ src/lineal_logaritmico.java
@@ -1,2 +1,55 @@
+import java.util.Arrays;
+import java.util.Random;
 public class lineal_logaritmico {
+    public static void mergeSort(int[] arr) {
+        if (arr == null || arr.length <= 1) {
+            return;
+        }
+        int mid = arr.length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[arr.length - mid];
+
+        System.arraycopy(arr, 0, left, 0, left.length);
+        System.arraycopy(arr, mid, right, 0, right.length);
+
+        mergeSort(left);
+        mergeSort(right);
+
+        merge(arr, left, right);
+    }
+
+    private static void merge(int[] arr, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                arr[k++] = left[i++];
+            } else {
+                arr[k++] = right[j++];
+            }
+        }
+        while (i < left.length) {
+            arr[k++] = left[i++];
+        }
+        while (j < right.length) {
+            arr[k++] = right[j++];
+        }
+    }
+
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+
+        int n = 100000; // Number of elements in the array
+        int[] arr = new int[n];
+        Random random = new Random();
+        for (int i = 0; i < n; i++) {
+            arr[i] = random.nextInt(100); // Generate random numbers between 0 and 99
+        }
+
+        mergeSort(arr);
+
+        long endTime = System.nanoTime();
+        long durationSeconds = (endTime - startTime);
+
+        System.out.println("Time taken: " + durationSeconds + " nanoseconds");
+    }
 }
